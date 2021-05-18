5aec8f3f3e1ba566fea4c199ba37e8dfd965a55a
Arthur Sondo <r.thur.light@gmail.com>
2021-05-18 17:27:42 +0000

    db access

--- database/database.js
+++ database/database.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql');
 
 const database = mysql.createConnection({
-    /*host: "sm9j2j5q6c8bpgyq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "ah057kfjbkolz4do",
-    password: "jaf89al1zf8viseg",
-    database: "qooalzvdkvh3byxb"*/
-    host: "localhost",
+    host: "bmlx3df4ma7r1yh4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "rk7rjyspxo5uqspw",
+    password: "aen6mbfb4rmkh672",
+    database: "xmmevf0y5gc49d9x"
+    /*host: "localhost",
     user: "root",
     password: "",
-    database: "trapflix"
+    database: "trapflix"*/
 });
 
 database.connect(function(err) {
